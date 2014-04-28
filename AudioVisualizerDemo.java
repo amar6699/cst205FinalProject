fefba69ed5440994dcef2ffdeca6c57f51c87ea9
amar6699 <Jamaral@csumb.edu>
2014-04-28 15:40:24 -0700

    Updated AudioVisualizer and MinimTest classses

--- AudioVisualizerDemo.java
+++ AudioVisualizerDemo.java
@@ -1,10 +1,29 @@
-package cst205FinalProject;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package AudioVisualizer;
+
+/**
+/*Name: Juan Amaral
+ *Date: April 28, 2014
+ *Title: Audio visualizer demo (main)
+ *Abstract: This class uses its main method to run the Audio visualizer 
+ *          and the MinimTest which will draw the waves
+ */
 public class AudioVisualizerDemo
 {
     public static void main(String[] args)
     {
         AudioVisualizer gui = new AudioVisualizer();
         gui.setVisible(true);
+        
+        MinimTest testPlayer = new MinimTest();
+        testPlayer.setVisible(true);
+        
     }
 }
+
+
